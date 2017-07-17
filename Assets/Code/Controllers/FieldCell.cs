939e7d844c5e3834d2a821a6ab2132b1066deee9
matryoshka <nazardjali@gmail.com>
2017-07-17 16:32:08 +0300

    add files

---
+++ Assets/Code/Controllers/FieldCell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldCell : MonoBehaviour {
+    [SerializeField] private Sprite _cross;
+    [SerializeField] private Sprite _circle;
+
+
+    public int currentState = 0; //TODO enum 
+
+    private void OnMouseDown() {
+
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        spriteRenderer.sprite = _cross;
+
+    }
+}
+
---
+++ Assets/Code/Controllers/FieldInitializeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldInitializeController : MonoBehaviour {
+    [SerializeField] private FieldCell _fieldCells;
+    [SerializeField] private GameObject field;
+
+    public float startX = -2.575f;
+    public float startY = 2.575f;
+
+    private void Start() {
+
+        for (int i = 0; i < 3; i++) {
+
+
+
+
+            for (int j = 0; j < 3; j++) {
+
+                FieldCell field = Instantiate(_fieldCells);
+                var curPos = field.transform.position;
+                curPos.x = startX + j * 2.575f;
+                curPos.y = startY - i * 2.575f;
+                if (field == null) {
+                    Debug.Log( i);
+                }
+                field.transform.position = curPos;
+
+            }
+
+        }
+
+    }
+}
