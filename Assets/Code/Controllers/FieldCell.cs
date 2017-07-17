28ce8af51536b6e237cb0ece3d132e29fca21c67
Yaroshenkom <gmaximum091@gmail.com>
2017-07-17 19:43:24 +0300

    Field cells have indexes now

--- Assets/Code/Controllers/FieldCell.cs
+++ Assets/Code/Controllers/FieldCell.cs
@@ -6,13 +6,40 @@ public class FieldCell : MonoBehaviour {
     [SerializeField] private Sprite _cross;
     [SerializeField] private Sprite _circle;
 
+    private int _placeX, _placeY;
+    public int PlaceX {
+        get {
+            return _placeX;
+        }
+        set {
+            _placeX = value;
+        }
+    }
+    public int PlaceY {
+        get {
+            return _placeY;
+        }
+        set {
+            _placeY = value;
+        }
+    }
 
     public int currentState = 0; //TODO enum 
-
+    public int index; //TODO private only with get
     private void OnMouseDown() {
+        
 
+    }
+
+    public void SetCircle() {
         SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
+        spriteRenderer.sprite = _circle;
+
+    }
+
+    public void SetCross() {
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.sprite = _cross;
 
     }
